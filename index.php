640807dfad66e1608a23a73e12f013fd9cc0139b
Landon Otis <landon@7bidaho.com>
2018-09-07 13:39:29 -0700

    Initial Upload
    Added first files. Probably won't have many more changes.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <header>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
